33e2e8dab64c3fd660eb267df7f30062cd2e2a7a
Abdullah UĞUZ <73312086+AbdullahUguz@users.noreply.github.com>
2023-02-14 01:13:14 +0300

    Update database.js
    fixed

--- utility/database.js
+++ utility/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
     'basic-blog-db',                             // database name
     'root',                            // root         
-    '12345',                           // password
+    '',                           //your db password
     {
         dialect:'mysql',
         host:'localhost'
